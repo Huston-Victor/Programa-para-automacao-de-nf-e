20d7c2a22fac7d6065b5a3ad26659fd3e7c27a6d
Huston <Hustonvictor81@gmail.com>
2026-06-30 14:48:56 -0300

    Primeiro commite para salvamento de pastas

---
+++ main.py
@@ -0,0 +1,15 @@
+import customtkinter as ctk
+
+#===========================
+#Criação da Janela Principal
+#===========================
+
+janela = ctk.CTk()
+
+
+
+#===========================
+#Looping da Janela Principal
+#===========================
+
+janela.mainloop()
